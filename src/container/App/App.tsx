046c653712dd91807b121d8d8f2ac83663ca5255
Koretski <nkoretski@gmail.com>
2022-12-13 11:03:42 +0100

    tiped import Header import Footer import Main in App.tsx

---
+++ src/container/App/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import Main from '../Main/Main'
+type Props = {}
+
+const App = (props: Props) => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
