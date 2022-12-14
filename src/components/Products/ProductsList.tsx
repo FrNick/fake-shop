33d4129e69f830ccf8882af2a9849d30a104a34b
Koretski <nkoretski@gmail.com>
2022-12-14 10:42:04 +0100

    add ProductsList and pages, sx=style, use sx instead of style

---
+++ src/components/Products/ProductsList.tsx
@@ -0,0 +1,20 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+type Props = {}
+
+const ProductsList = (props: Props) => {
+    return (
+        <>
+            <Typography
+                align="center"
+                variant="h4"
+                sx={{ margin: '25px 0', textTransform: 'uppercase' }}
+            >
+                Products List
+            </Typography>
+        </>
+    )
+}
+
+export default ProductsList
--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,9 +1,17 @@
+import { Container } from '@mui/system'
+import Home from 'pages/Home/Home'
 import React from 'react'
 
 type Props = {}
 
 const Main = (props: Props) => {
-    return <div>Main</div>
+    return (
+        <>
+            <Container maxWidth="lg">
+                <Home />
+            </Container>
+        </>
+    )
 }
 
 export default Main
