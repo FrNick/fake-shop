69b3d2b46336b1308da3ff64974db93170b344ce
Koretski <nkoretski@gmail.com>
2022-12-14 20:19:41 +0100

    add ProductListItem.css

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -2,8 +2,7 @@ import { CardContent } from '@mui/material'
 import { Button } from '@mui/material'
 import { CardActions } from '@mui/material'
 import { Card } from '@mui/material'
-import React from 'react'
-
+import './ProductListItem.css'
 type Props = {
     name: string
     description: string
@@ -22,8 +21,8 @@ const ProductListItem = (props: Props) => {
                 <div>Capacity: {props.capacity} Gb</div>
                 <div>Price:${props.price}</div>
             </CardContent>
-            <CardActions>
-                <Button>Add to cart</Button>
+            <CardActions className="btn-wrap">
+                <Button variant="outlined">Add to cart</Button>
             </CardActions>
         </Card>
     )
