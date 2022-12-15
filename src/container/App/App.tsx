dd3e348224e13df06e6ce53c1a6542a4eeea386b
Koretski <nkoretski@gmail.com>
2022-12-15 10:26:48 +0100

    add StyledEngineProvider injectFirst to App.tsx

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -3,16 +3,17 @@ import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
 import Footer from '../Footer/Footer'
 import Main from '../Main/Main'
+import { StyledEngineProvider } from '@mui/material'
 type Props = {}
 
 const App = (props: Props) => {
     return (
-        <>
+        <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header />
             <Main />
             <Footer />
-        </>
+        </StyledEngineProvider>
     )
 }
 
