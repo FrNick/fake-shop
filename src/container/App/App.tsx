bdb45bd0fa7a8cf5e9382473316c38271b2c4431
Koretski <nkoretski@gmail.com>
2022-12-28 10:01:49 +0100

    add productId, methods key and map

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -7,25 +7,25 @@ import { StyledEngineProvider } from '@mui/material'
 import 'style/style.scss'
 
 type Props = {}
-type CartData = {
-    totalCount: number
-    totalPrice: number
+type AppProps = {
+    [id: number]: number
 }
+
 const App = (props: Props) => {
-    const [cartData, setcartData] = useState<CartData>({
-        totalCount: 0,
-        totalPrice: 0,
+    const [productsInCart, setproductsInCart] = useState<AppProps>({
+        1: 5,
+        2: 5,
     })
     const addProductToCart = (count: number, price: number) => {
-        setcartData((prevState: CartData) => ({
-            totalCount: prevState.totalCount + count,
-            totalPrice: prevState.totalPrice + count * price,
-        }))
+        // setcartData((prevState: CartData) => ({
+        //     totalCount: prevState.totalCount + count,
+        //     totalPrice: prevState.totalPrice + count * price,
+        // }))
     }
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
-            <Header cartData={cartData} />
+            <Header productsInCart={productsInCart} />
             <Main addProductToCart={addProductToCart} />
             <Footer />
         </StyledEngineProvider>
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -10,12 +10,11 @@ import CartHeader from 'components/Cart/CartHeader'
 import './Header.scss'
 import logo from 'assets/logo.svg'
 type Props = {
-    cartData: {
-        totalCount: number
-        totalPrice: number
+    productsInCart: {
+        [id: number]: number
     }
 }
-const Header = ({ cartData }: Props) => {
+const Header = ({ productsInCart }: Props) => {
     return (
         <>
             <AppBar position="static" className="app-bar">
@@ -38,10 +37,7 @@ const Header = ({ cartData }: Props) => {
                             <img src={logo} alt="fake-shop" />
                         </Typography>
                         <Menu />
-                        <CartHeader
-                            totalCount={cartData.totalCount}
-                            totalPrice={cartData.totalPrice}
-                        />
+                        <CartHeader productsInCart={productsInCart} />
                     </Toolbar>
                 </Container>
             </AppBar>
