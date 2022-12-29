a3742a3a249674a6e6ef58255fcb4ddd35ce2253
Koretski <nkoretski@gmail.com>
2022-12-29 11:21:50 +0100

    add id, count

--- src/components/Cart/CartHeader.tsx
+++ src/components/Cart/CartHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { keys } from 'lodash'
-
+import productsArray from 'components/Products/ProductsArray'
 type Props = {
     productsInCart: {
         [id: number]: number
@@ -12,7 +12,8 @@ const CartHeader = ({ productsInCart }: Props) => {
         <div>
             {keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productId}: {productsInCart[parseInt(productId)]}
+                    {productsArray[parseInt(productId) - 1].name}:{' '}
+                    {productsInCart[parseInt(productId)]}
                 </div>
             ))}
         </div>
